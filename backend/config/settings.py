8cedd6379a4db76be97c2ff90b9db1dad740e450
JuanASanta <sleonjuanantonio@gmail.com>
2026-06-23 12:07:52 +0200

    SimpleJWT authentication configure

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,4 +132,7 @@ AUTH_USER_MODEL = 'users.CustomUser'  # Indica que estamos usando nuestro modelo
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,8 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Login y Autenticación mediante JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Rutas de mis apps
     path("api/users/", include("apps.users.urls")),
     path("api/habits/", include("apps.habits.urls")),
     path("api/habitscompletion/", include("apps.progress.urls")),
